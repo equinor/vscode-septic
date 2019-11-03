9fd15785e19eb9fd373cec5be78d72131fc4b008
Einar S. Idsø <2356425+einarsi@users.noreply.github.com>
2019-11-03 16:20:57 +0100

    Added workspaceSymbolProvider

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,22 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import SepticFoldingProvider from './foldingProvider';
 import SepticDocumentSymbolProvider from './documentSymbolProvider';
+import SepticFoldingProvider from './foldingProvider';
+import SepticWorkspaceSymbolProvider from './workspaceSymbolProvider';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(registerSepticLanguageFeatures());
+	const symbolProvider = new SepticDocumentSymbolProvider();
+
+	context.subscriptions.push(registerSepticLanguageFeatures(symbolProvider));
 	//console.log('Congratulations, your extension is now active!');
 }
 
 function registerSepticLanguageFeatures(
+	symbolProvider: SepticDocumentSymbolProvider
 ): vscode.Disposable {
 	const selector: vscode.DocumentSelector = [
 		{ language: 'septic', scheme: 'file' },
@@ -20,8 +24,9 @@ function registerSepticLanguageFeatures(
 	];
 
 	return vscode.Disposable.from(
+		vscode.languages.registerDocumentSymbolProvider(selector, symbolProvider),
 		vscode.languages.registerFoldingRangeProvider(selector, new SepticFoldingProvider()),
-		vscode.languages.registerDocumentSymbolProvider(selector, new SepticDocumentSymbolProvider())
+		vscode.languages.registerWorkspaceSymbolProvider(new SepticWorkspaceSymbolProvider(symbolProvider))
 	);
 }
 
---
+++ src/workspaceSymbolProvider.ts
@@ -0,0 +1,172 @@
+
+import * as vscode from 'vscode';
+import { Disposable } from './util/dispose';
+import { Lazy, lazy } from './util/lazy';
+import SepticDocumentSymbolProvider from './documentSymbolProvider';
+import { SkinnyTextDocument, SkinnyTextLine } from './tableOfContentsProvider';
+import { flatten } from './util/arrays';
+
+export function isSepticFile(document: vscode.TextDocument) {
+	console.log('isSepticFile')
+	return document.languageId === 'septic';
+}
+
+export interface WorkspaceSepticDocumentProvider {
+	getAllSepticDocuments(): Thenable<Iterable<SkinnyTextDocument>>;
+
+	readonly onDidChangeSepticDocument: vscode.Event<SkinnyTextDocument>;
+	readonly onDidCreateSepticDocument: vscode.Event<SkinnyTextDocument>;
+	readonly onDidDeleteSepticDocument: vscode.Event<vscode.Uri>;
+}
+
+class VSCodeWorkspaceSepticDocumentProvider extends Disposable implements WorkspaceSepticDocumentProvider {
+
+	private readonly _onDidChangeSepticDocumentEmitter = this._register(new vscode.EventEmitter<SkinnyTextDocument>());
+	private readonly _onDidCreateSepticDocumentEmitter = this._register(new vscode.EventEmitter<SkinnyTextDocument>());
+	private readonly _onDidDeleteSepticDocumentEmitter = this._register(new vscode.EventEmitter<vscode.Uri>());
+
+	private _watcher: vscode.FileSystemWatcher | undefined;
+
+	async getAllSepticDocuments() {
+		console.log('getAllSepticDocuments')
+		const resources = await vscode.workspace.findFiles('**/*.cnfg');
+		const docs = await Promise.all(resources.map(doc => this.getSepticDocument(doc)));
+		console.log(docs);
+		return docs.filter(doc => !!doc) as SkinnyTextDocument[];
+	}
+
+	public get onDidChangeSepticDocument() {
+		console.log('onDidChangeSepticDocument')
+		this.ensureWatcher();
+		return this._onDidChangeSepticDocumentEmitter.event;
+	}
+
+	public get onDidCreateSepticDocument() {
+		console.log('onDidCreateSepticDocument')
+		this.ensureWatcher();
+		return this._onDidCreateSepticDocumentEmitter.event;
+	}
+
+	public get onDidDeleteSepticDocument() {
+		console.log('onDidDeleteSepticDocument')
+		this.ensureWatcher();
+		return this._onDidDeleteSepticDocumentEmitter.event;
+	}
+
+	private ensureWatcher(): void {
+		console.log('ensureWatcher')
+		if (this._watcher) {
+			return;
+		}
+
+		this._watcher = this._register(vscode.workspace.createFileSystemWatcher('**/*.cnfg'));
+
+		this._watcher.onDidChange(async resource => {
+			const document = await this.getSepticDocument(resource);
+			if (document) {
+				this._onDidChangeSepticDocumentEmitter.fire(document);
+			}
+		}, null, this._disposables);
+
+		this._watcher.onDidCreate(async resource => {
+			const document = await this.getSepticDocument(resource);
+			if (document) {
+				this._onDidCreateSepticDocumentEmitter.fire(document);
+			}
+		}, null, this._disposables);
+
+		this._watcher.onDidDelete(async resource => {
+			this._onDidDeleteSepticDocumentEmitter.fire(resource);
+		}, null, this._disposables);
+
+		vscode.workspace.onDidChangeTextDocument(e => {
+			if (isSepticFile(e.document)) {
+				this._onDidChangeSepticDocumentEmitter.fire(e.document);
+			}
+		}, null, this._disposables);
+	}
+
+	private async getSepticDocument(resource: vscode.Uri): Promise<SkinnyTextDocument | undefined> {
+		console.log('getSepticDocument: ' + resource)
+		const matchingDocuments = vscode.workspace.textDocuments.filter((doc) => doc.uri.toString() === resource.toString());
+		if (matchingDocuments.length !== 0) {
+			return matchingDocuments[0];
+		}
+
+		const bytes = await vscode.workspace.fs.readFile(resource);
+
+		const text = Buffer.from(bytes).toString('ascii');
+
+		const lines: SkinnyTextLine[] = [];
+		const parts = text.split(/(\r?\n)/);
+		const lineCount = Math.floor(parts.length / 2) + 1;
+		for (let line = 0; line < lineCount; line++) {
+			lines.push({
+				text: parts[line * 2]
+			});
+		}
+		console.log(lines);
+		return {
+			uri: resource,
+			version: 0,
+			lineCount: lineCount,
+			lineAt: (index) => {
+				return lines[index];
+			},
+			getText: () => {
+				return text;
+			}
+		};
+	}
+}
+
+export default class SepticWorkspaceSymbolProvider extends Disposable implements vscode.WorkspaceSymbolProvider {
+	private _symbolCache = new Map<string, Lazy<Thenable<vscode.SymbolInformation[]>>>();
+	private _symbolCachePopulated: boolean = false;
+
+	public constructor(
+		private _symbolProvider: SepticDocumentSymbolProvider,
+		private _workspaceSepticDocumentProvider: WorkspaceSepticDocumentProvider = new VSCodeWorkspaceSepticDocumentProvider()
+	) {
+		super();
+	}
+
+	public async provideWorkspaceSymbols(query: string): Promise<vscode.SymbolInformation[]> {
+		console.log('provideWorkspaceSymbols');
+		if (!this._symbolCachePopulated) {
+			await this.populateSymbolCache();
+			this._symbolCachePopulated = true;
+
+			this._workspaceSepticDocumentProvider.onDidChangeSepticDocument(this.onDidChangeDocument, this, this._disposables);
+			this._workspaceSepticDocumentProvider.onDidCreateSepticDocument(this.onDidChangeDocument, this, this._disposables);
+			this._workspaceSepticDocumentProvider.onDidDeleteSepticDocument(this.onDidDeleteDocument, this, this._disposables);
+		}
+
+		const allSymbolsSets = await Promise.all(Array.from(this._symbolCache.values()).map(x => x.value));
+		const allSymbols = flatten(allSymbolsSets);
+		return allSymbols.filter(symbolInformation => symbolInformation.name.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+	}
+
+	public async populateSymbolCache(): Promise<void> {
+		const septicDocumentUris = await this._workspaceSepticDocumentProvider.getAllSepticDocuments();
+		for (const document of septicDocumentUris) {
+			this._symbolCache.set(document.uri.fsPath, this.getSymbols(document));
+		}
+	}
+
+	private getSymbols(document: SkinnyTextDocument): Lazy<Thenable<vscode.SymbolInformation[]>> {
+		return lazy(async () => {
+			return this._symbolProvider.provideDocumentSymbolInformation(document);
+		});
+	}
+
+	private onDidChangeDocument(document: SkinnyTextDocument) {
+		console.log('onDidChangeDocument');
+		this._symbolCache.set(document.uri.fsPath, this.getSymbols(document));
+	}
+
+	private onDidDeleteDocument(resource: vscode.Uri) {
+		console.log('onDidDeleteDocument')
+		this._symbolCache.delete(resource.fsPath);
+	}
+}
