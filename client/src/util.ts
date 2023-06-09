2d554c97ddef5e40041e74c73285a41bc933d603
Vetle Andersen Koppergård <89847536+vetlek@users.noreply.github.com>
2023-06-09 13:09:11 +0200

    fix: bug with wrong files in context (#178)

---
+++ client/src/util.ts
@@ -0,0 +1,15 @@
+import * as path from "path";
+
+export function getSearchPattern(workspacePath: string, templatePath: string) {
+    let baseWorkspace = path.basename(workspacePath);
+    let parts: string[] = [];
+    let temp = templatePath;
+    while (path.basename(temp) !== baseWorkspace) {
+        parts.push(path.basename(temp));
+        temp = path.dirname(temp);
+    }
+
+    parts = parts.reverse();
+
+    return "**/" + parts.join("/") + "/**.cnfg";
+}
