a332d1f5261458aeb1a667c9d8fead1c05639b93
Vetle Koppergård <89847536+vetlek@users.noreply.github.com>
2023-07-12 15:47:07 +0200

    Test

--- client/src/test/formatting.test.ts
+++ client/src/test/formatting.test.ts
@@ -46,8 +46,7 @@ function compareFiles(expectedContent: string, actualContent: string) {
     ) {
         const e = expectedLines[i];
         const a = actualLines[i];
-        console.log(`${i} Expected: ${e}`);
-        console.log(`${i} Actual:   ${a}`);
+        expect(e, `Difference at line ${i}`).to.be.equal(a);
     }
 
     expect(
