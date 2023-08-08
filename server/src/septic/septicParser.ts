ec6431cf3db0d69ed78a96a32dd3adedbb3746e6
Vetle Andersen Koppergård <89847536+vetlek@users.noreply.github.com>
2023-08-08 14:38:41 +0200

    Remove unused import

--- server/src/septic/septicParser.ts
+++ server/src/septic/septicParser.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { CancellationToken, Trace } from "vscode-languageserver";
+import { CancellationToken } from "vscode-languageserver";
 import { Parser, IToken, ParserError } from "./parser";
 import { SepticCnfg } from "./septicCnfg";
 import { SepticToken, SepticTokenType } from "./septicTokens";
