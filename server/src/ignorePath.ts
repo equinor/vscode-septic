6301a5096e76619c4f796bc161a6a5e40e45b416
Vetle Andersen Koppergård <89847536+vetlek@users.noreply.github.com>
2023-09-01 15:01:34 +0200

    Update

--- server/src/ignorePath.ts
+++ server/src/ignorePath.ts
@@ -5,20 +5,10 @@ export async function getIgnorePatterns(
     connection: Connection,
     settingsManager: SettingsManager
 ): Promise<string[]> {
-    const workspaceFolders: WorkspaceFolder[] | null =
-        await connection.workspace.getWorkspaceFolders();
-    if (!workspaceFolders) {
-        return [];
-    }
-    const workspaceFoldersNames = workspaceFolders.map((wf) => wf.name);
     const settings = await settingsManager.getSettings();
     const ignoredPaths = settings?.ignored.paths ?? [];
     const ignorePatterns: string[] = [];
     for (let path of ignoredPaths) {
-        let root = path.split("/")[0];
-        if (!workspaceFoldersNames.includes(root)) {
-            continue;
-        }
         ignorePatterns.push(pathToRegex(path));
     }
     return ignorePatterns;
@@ -36,7 +26,7 @@ export function isPathIgnored(path: string, ignorePatterns: string[]): boolean {
 
 function pathToRegex(path: string): string {
     // Convert path to a regex pattern
-    let workspacePath = "*/" + path;
+    let workspacePath = "*" + path;
     let pattern = workspacePath.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&"); // Escape special characters
     pattern = pattern.replace(/\\\*/g, ".*"); // Replace * with .*
     pattern = pattern.replace(/\\\?/g, "."); // Replace ? with .
