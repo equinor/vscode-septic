e853829c81523b5e7850d8876650e6813c1f508a
Vetle Koppergård <89847536+vetlek@users.noreply.github.com>
2024-04-08 14:13:10 +0200

    Format list in snippets correctly

--- scripts/src/snippets.py
+++ scripts/src/snippets.py
@@ -6,6 +6,11 @@ import yaml
 
 public_path = Path("./public")
 
+indents_attribute_value = 17
+spaces_between_int_values = 6
+spaces_first_int_value = 5
+spaces_between_values = 2
+
 
 @dataclass
 class Snippet:
@@ -21,7 +26,7 @@ def create_snippet(obj: dict) -> Snippet:
     for attr in obj["attributes"]:
         if attr["noCnfg"] == "true":
             continue
-        body.append(format_attribute(attr))
+        body.extend(format_attribute(attr))
     description = obj["description"]
     return Snippet(prefix, body, description)
 
@@ -47,28 +52,47 @@ def format_attribute(attribute: dict):
         name = attribute["name"]
     indents_attribute_delimiter = 14
     indents_line = max(indents_attribute_delimiter - len(name), 0)
-    return (
-        " " * indents_line
-        + name
-        + "=  "
-        + format_attribute_value(attribute["default"], attribute["list"])
+    attribute_def = " " * indents_line + name + "=  "
+    attribute_values = format_attribute_value(
+        attribute["default"], attribute["list"], attribute["dataType"]
     )
+    attribute_values[0] = attribute_def + str(attribute_values[0])
+    return attribute_values
 
 
-def format_attribute_value(values: List[str], is_list: str):
+def format_attribute_value(values: List[str], is_list: str, datatype: str) -> List[str]:
     if not values:
-        return ""
-    if is_list == "true":
-        return f"{len(values)}" + "  " + "  ".join(values)
-    return values[0]
+        return [""]
+    if is_list == "true" and datatype.lower() in ["string", "variable"]:
+        return format_string_list(values)
+    elif is_list == "true" and datatype.lower() == "int":
+        return format_int_list(values)
+    elif is_list == "true":
+        sep = " " * spaces_between_values
+        return [f"{len(values)}" + sep + sep.join(values)]
+    return [values[0]]
 
 
-def is_int(s):
-    try:
-        int(s)
-        return True
-    except ValueError:
-        return False
+def format_string_list(values: List[str]):
+    lines = [len(values)]
+    current_line = " " * indents_attribute_value
+    for ind, val in enumerate(values):
+        if ind % 5 == 0 and ind != 0:
+            lines.append(current_line.rstrip())
+            current_line = " " * indents_attribute_value
+        current_line += val + " " * spaces_between_values
+    lines.append(current_line)
+    return lines
+
+
+def format_int_list(values: List[str]):
+    line = f"{len(values)}"
+    for ind, val in enumerate(values):
+        if ind == 0:
+            line += " " * max(1, spaces_first_int_value - len(val)) + val
+        else:
+            line += " " * max(1, spaces_between_int_values - len(val)) + val
+    return [line]
 
 
 def generate_snippets(version: str):
