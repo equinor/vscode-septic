cf71b97bfb3f2a7a1653eb28c46326a6563d5078
Vetle Koppergård <89847536+vetlek@users.noreply.github.com>
2024-10-14 08:56:52 +0200

    Support refs in algs with jinja for and if

--- server/src/util/str.ts
+++ server/src/util/str.ts
@@ -9,3 +9,32 @@ export function isPureJinja(str: string) {
 export function isAlphaNumeric(char: string): boolean {
     return /^[a-zA-Z0-9]$/.test(char);
 }
+
+export function removeJinjaLoopsAndIfs(input: string): { strippedString: string, positionsMap: number[] } {
+    let positionsMap: number[] = [];
+    let regex = /{%\s*for .*?\s*%}|{%\s*endfor\s*%}|{%\s*if .*?\s*%}.*?{%\s*endif\s*%}/g
+    let strippedString = input.replace(regex, (match, offset) => {
+        positionsMap.push(offset, offset + match.length);
+        return '';
+    });
+    return { strippedString, positionsMap };
+}
+
+export function transformPositionsToOriginal(positions: number[], positionsMap: number[]): number[] {
+    let originalPositions: number[] = [];
+    for (let pos of positions) {
+        let orignalPosition = pos;
+        for (let i = 0; i < positionsMap.length; i += 2) {
+            if (orignalPosition >= positionsMap[i] && orignalPosition <= positionsMap[i + 1]) {
+                orignalPosition += positionsMap[i + 1] - positionsMap[i];
+            } else if (i + 2 >= positionsMap.length) {
+                orignalPosition += positionsMap[i + 1] - positionsMap[i];
+            }
+            if (i + 2 >= positionsMap.length || orignalPosition < positionsMap[i + 2]) {
+                originalPositions.push(orignalPosition);
+                break;
+            }
+        }
+    }
+    return originalPositions;
+}
