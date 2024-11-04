8961f8ffeb2fc2dd79f8b97e7ce9e1a4d33366df
Vetle Koppergård <89847536+vetlek@users.noreply.github.com>
2024-11-04 15:17:55 +0100

    Update ignore paths parsing

--- server/src/ignorePath.ts
+++ server/src/ignorePath.ts
@@ -1,9 +1,9 @@
-import { Connection, WorkspaceFolder } from "vscode-languageserver";
+import { Connection, } from 'vscode-languageserver';
 import { SettingsManager } from "./settings";
 
 
 export interface IgnoredPaths {
-    regex: string;
+    regex: RegExp;
     codes: string[];
 }
 
@@ -15,33 +15,31 @@ export async function getIgnorePatterns(
     if (!settings) {
         return [];
     }
+    const folders = await connection.workspace.getWorkspaceFolders();
+    const workspace = folders ? folders[0].uri : "*";
     const ignoredPaths: { [key: string]: string[] } = settings.ignored.paths;
     const ignorePatterns: IgnoredPaths[] = [];
     Object.keys(ignoredPaths).forEach((path) => {
-        ignorePatterns.push({ regex: pathToRegex(path), codes: ignoredPaths[path] });
+        ignorePatterns.push({ regex: pathToRegex(workspace, path), codes: ignoredPaths[path] });
     })
     return ignorePatterns;
 }
 
 export function getIgnoredCodes(path: string, ignorePatterns: IgnoredPaths[]): string[] | undefined {
     for (const ignorePattern of ignorePatterns) {
-        const regexPattern = new RegExp(ignorePattern.regex);
-        if (regexPattern.test(path)) {
+        if (ignorePattern.regex.test(path)) {
             return ignorePattern.codes;
         }
     }
     return undefined;
 }
 
-function pathToRegex(path: string): string {
-    // Convert path to a regex pattern
-    let workspacePath = "*" + path;
-    let pattern = workspacePath.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&"); // Escape special characters
-    pattern = pattern.replace(/\\\*/g, ".*"); // Replace * with .*
-    pattern = pattern.replace(/\\\?/g, "."); // Replace ? with .
-
-    // Ensure the pattern matches the whole path
+function pathToRegex(workspace: string, path: string): RegExp {
+    path = path.replace(/^\./, "").replace(/^\//, "");
+    let absPath = workspace + "/" + path;
+    let pattern = absPath.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&"); // Escape special characters
+    pattern = pattern.replace(/\\\*/g, ".*");
+    pattern = pattern.replace(/\\\?/g, ".");
     pattern = `^${pattern}$`;
-
-    return pattern;
+    return new RegExp(pattern)
 }
