f073719c1d9d2579ae76180c53008243b01a0f00
Vetle Andersen Koppergård <VETK@equinor.com>
2024-12-02 14:27:52 +0100

    Move request handlers to separate file

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -5,8 +5,6 @@
 
 import * as vscode from "vscode";
 import * as path from "path";
-import * as protocol from "./protocol";
-import { getSearchPattern } from "./util";
 import {
     LanguageClient,
     LanguageClientOptions,
@@ -17,6 +15,7 @@ import { calcChat } from './chat';
 import { FileTelemetrySender } from './logger';
 import * as fs from 'fs';
 import { registerAllCommands } from './commands';
+import { registerRequestHandlers } from "./requests";
 
 let client: LanguageClient;
 
@@ -54,34 +53,9 @@ export function activate(context: vscode.ExtensionContext) {
         serverOptions,
         clientOptions
     );
-    client.onRequest(protocol.globFiles, async (params) => {
-        const parsedUri = vscode.Uri.parse(params.uri);
-        const folder = vscode.workspace.getWorkspaceFolder(parsedUri);
-        if (!folder) {
-            return [];
-        }
-        const pattern = getSearchPattern(folder.uri.path, parsedUri.path);
-        const files = await vscode.workspace.findFiles(pattern);
-        return files
-            .filter((f) => f.fsPath.includes(folder.uri.fsPath))
-            .map((f) => f.toString());
-    });
-
-    client.onRequest(
-        protocol.fsReadFile,
-        async (e): Promise<number[]> => {
-            const uri = vscode.Uri.parse(e.uri);
-            return Array.from(await vscode.workspace.fs.readFile(uri));
-        }
-    );
-
-    client.onRequest(protocol.findYamlFiles, async () => {
-        return (await vscode.workspace.findFiles(`**/*.yaml`)).map((f) =>
-            f.toString()
-        );
-    });
 
     registerAllCommands(context, client);
+    registerRequestHandlers(client);
 
     if (!fs.existsSync(context.logUri.fsPath)) {
         fs.mkdirSync(context.logUri.fsPath);
