062a5ff6d033877ad40a69917d6b2192feb92062
Vetle Koppergård <89847536+vetlek@users.noreply.github.com>
2024-12-06 12:47:33 +0100

    Add commands to chat participant

--- client/src/toolsPrompt.tsx
+++ client/src/toolsPrompt.tsx
@@ -24,6 +24,8 @@ import {
 import { ToolResult } from '@vscode/prompt-tsx/dist/base/promptElements';
 import * as vscode from 'vscode';
 import { isTsxToolUserMetadata } from './chatParticipant';
+import { LanguageClient } from 'vscode-languageclient/node';
+import * as protocol from './protocol';
 
 export interface ToolCallRound {
 	response: string;
@@ -33,12 +35,26 @@ export interface ToolCallRound {
 export interface ToolUserProps extends BasePromptElementProps {
 	request: vscode.ChatRequest;
 	context: vscode.ChatContext;
+	client: LanguageClient;
 	toolCallRounds: ToolCallRound[];
 	toolCallResults: Record<string, vscode.LanguageModelToolResult>;
 }
 
 export class ToolUserPrompt extends PromptElement<ToolUserProps, void> {
 	render(_state: void, _sizing: PromptSizing) {
+		let instructions;
+		switch (this.props.request.command) {
+			case 'calculation':
+				instructions = <CalculationInstructions priority={100}/>;
+				break;
+			case 'scg':
+				instructions = <ScgInstructions client={this.props.client} priority={100}/>;
+				break;
+			// Add more cases here for different commands
+			default:
+				instructions = <UserMessage priority={100}>No specific instructions available for this command.</UserMessage>;
+		}
+
 		return (
 			<>
 				<History context={this.props.context} priority={10} />
@@ -46,46 +62,7 @@ export class ToolUserPrompt extends PromptElement<ToolUserProps, void> {
 					references={this.props.request.references}
 					priority={20}
 				/>
-				<UserMessage priority={100}>
-					Calculation instructions: <br />
-					- Septic configuration supports execution of calculations. A calculation is written as a string inside a calculation object and the content of the string is executed by the MPC.<br />
-					- Format of calculation object: <br />
-					CalcPvr:   %calculation name % <br />
-					Text1=  "% description %" <br />
-					Text2=  "" <br />
-					Alg= "% calculation %"
-					- The calculation is written in a simple language that supports basic arithmetic operations, variables, and functions. The following rules apply and most be strictly followed: <br />
-					%%% start rules %%% <br />
-					- Supported operators: +, -, *, /, % <br />
-					- Supported comparison operators: ==, {'>'}, {'>='}, {'<='}, {'<'} <br />
-					- Supported logical functions: and(condition1, condition2, ..., conditionN), or(condition1, condition2, ..., conditionN), not(condition) <br />
-					- Grouping with parentheses: (...) <br />
-					- Variables: Refers to objects in the configuration file. It is allowed with jinja expressions in variable names, but not only jinja. Example: {`{{ Jinja }}`}VariableName <br />
-					- All non-zero values are considered true <br />
-					- There is a set of available functions. All functions returns a float <br />
-					- Arguments to function are separated by commas <br />
-					- Calculations are insensitive to whitespace <br />
-					- The if function is written as follows: if(condition, true_value, false_value) both true_value and false_value are evaluated, but only the correct one is returned, thus functions that sets values must not be used within the if function and instead get the result of the if function as input. <br />
-					%%% end rules %%% <br />
-					%%% examples %%% <br />
-					Examples of correct calculations: <br />
-					setmeas(Var1, if(Var2 {'>'} Var1, Var2, Var3)) <br />
-					setmode(Var1, if(getmode(Var1) {'>'} 3, 3, 0),1)<br />
-					and(Var2 {'>'} Var1, Var3 == 1)<br />
-					or(Var2 {'>'} Var1, Var3 == 1)<br />
-					Examples of incorrect calculations: <br />
-					if(Var2 {'>'} Var1, setmeas(Var1, Var2), Var3)<br />
-					if(getmode(Var1) {'>'} 3, setmode(Var1, 3), setmode(Var1, 1))<br />
-					Var2 {'>'} Var1 and Var3 == 1<br />
-					Var2 {'>'} Var1 or Var3 == 1<br />
-					%%% end examples %%% <br />
-					- Use the available tools to get the relevant functions and variables. Never ask for confirmation to use the tools. <br />
-					- Always validate all calculations. Repeat the validation after each change. Never ask for confirmation to use the tool. <br />
-					- If the calculation require new variables not in the context, provide a list of the new variables that needs to be added when validating the calculation. <br />
-					- The calculation result is stored in a variable if a variable with the same name as the calculation object is defined in the configuration as an Evr. Always add new variables to store the result of the calculations <br />
-					- Summarize the all calculations and new variables when the results are ready. Format new variables as Evr objects. Group variables and calculation such that it is easy for the user to insert into the configuration  <br />
-					- Always validate all calculations. Repeat the validation after each change. Never ask for confirmation to use the tool. <br />
-				</UserMessage>
+				{instructions}
 				<UserMessage priority={100}>
 					Instructions: <br />
 					- The user will ask a question, or ask you to perform a task, and it may
@@ -324,6 +301,118 @@ class Tag extends PromptElement<TagProps> {
 				</>
 				{'</' + name + '>'}<br />
 			</>
+		);
+	}
+}
+
+
+class CalculationInstructions extends PromptElement<object> {
+	render() {
+		return (
+			<UserMessage priority={this.props.priority}>
+					Calculation instructions: <br />
+					- Septic configuration supports execution of calculations. A calculation is written as a string inside a calculation object and the content of the string is executed by the MPC.<br />
+					- Format of calculation object: <br />
+					CalcPvr:   %calculation name % <br />
+					Text1=  "% description %" <br />
+					Text2=  "" <br />
+					Alg= "% calculation %"
+					- The calculation is written in a simple language that supports basic arithmetic operations, variables, and functions. The following rules apply and most be strictly followed: <br />
+					%%% start rules %%% <br />
+					- Supported operators: +, -, *, /, % <br />
+					- Supported comparison operators: ==, {'>'}, {'>='}, {'<='}, {'<'} <br />
+					- Supported logical functions: and(condition1, condition2, ..., conditionN), or(condition1, condition2, ..., conditionN), not(condition) <br />
+					- Grouping with parentheses: (...) <br />
+					- Variables: Refers to objects in the configuration file. It is allowed with jinja expressions in variable names, but not only jinja. Example: {`{{ Jinja }}`}VariableName <br />
+					- All non-zero values are considered true <br />
+					- There is a set of available functions. All functions returns a float <br />
+					- Arguments to function are separated by commas <br />
+					- Calculations are insensitive to whitespace <br />
+					- The if function is written as follows: if(condition, true_value, false_value) both true_value and false_value are evaluated, but only the correct one is returned, thus functions that sets values must not be used within the if function and instead get the result of the if function as input. <br />
+					%%% end rules %%% <br />
+					%%% examples %%% <br />
+					Examples of correct calculations: <br />
+					setmeas(Var1, if(Var2 {'>'} Var1, Var2, Var3)) <br />
+					setmode(Var1, if(getmode(Var1) {'>'} 3, 3, 0),1)<br />
+					and(Var2 {'>'} Var1, Var3 == 1)<br />
+					or(Var2 {'>'} Var1, Var3 == 1)<br />
+					Examples of incorrect calculations: <br />
+					if(Var2 {'>'} Var1, setmeas(Var1, Var2), Var3)<br />
+					if(getmode(Var1) {'>'} 3, setmode(Var1, 3), setmode(Var1, 1))<br />
+					Var2 {'>'} Var1 and Var3 == 1<br />
+					Var2 {'>'} Var1 or Var3 == 1<br />
+					%%% end examples %%% <br />
+					- Use the available tools to get the relevant functions and variables. Never ask for confirmation to use the tools. <br />
+					- Always validate all calculations. Repeat the validation after each change. Never ask for confirmation to use the tool. <br />
+					- If the calculation require new variables not in the context, provide a list of the new variables that needs to be added when validating the calculation. <br />
+					- The calculation result is stored in a variable if a variable with the same name as the calculation object is defined in the configuration as an Evr. Always add new variables to store the result of the calculations <br />
+					- Summarize the all calculations and new variables when the results are ready. Format new variables as Evr objects. Group variables and calculation such that it is easy for the user to insert into the configuration  <br />
+					- Always validate all calculations. Repeat the validation after each change. Never ask for confirmation to use the tool. <br />
+				</UserMessage>
+		);
+	}
+}
+
+interface ScgInstructionsProps extends BasePromptElementProps {
+	client: LanguageClient;
+}
+
+class ScgInstructions extends PromptElement<ScgInstructionsProps> {
+	async render() {
+		const uri = vscode.window.activeTextEditor?.document.uri.toString();
+		const context = await this.props.client.sendRequest(protocol.getContext, {uri: uri});
+		const fileContent = await vscode.workspace.fs.readFile(vscode.Uri.parse(context));
+		const fileText = Buffer.from(fileContent).toString('utf8');
+		return (
+			<UserMessage priority={this.props.priority}>
+					Septic Config Generator: <br />
+					- Septic Config Generator (SCG) is a tool that enables generating Septic config files based on a set of templates that use information from one or more sources<br />
+					- SCG is based on the MiniJinja template engine that use the syntax and behavior of the Jinja2 template engine<br />
+					- A context is defined by a configuration file in yaml format that contains the information that the SCG tool need to generate the config files<br />
+					%%% start example scg config %%% <br />
+					outputfile: example.cnfg <br />
+					templatepath: templates <br />
+					adjustspacing: true <br />
+					verifycontent: true <br />
+					<br />
+					counters: <br />
+					- name: mycounter <br />
+						value: 0 <br />
+						<br />
+					sources: <br />
+					- filename: example.xlsx <br />
+						id: wells <br />
+						sheet: Sheet1 <br />
+					- filename: example.csv <br />
+						id: flowlines <br />
+						delimiter: ";" <br />
+						<br />
+					layout: <br />
+					- name: 010_System.cnfg <br />
+					- name: 020_SopcProc.cnfg <br />
+					- name: 030_SopcProc_well.cnfg <br />
+						source: wells <br />
+						include: <br />
+						- D01 <br />
+						- D02 <br />
+					- name: 040_SopcProc_flowline.cnfg <br />
+						source: flowlines <br />
+					%%% end example scg config %%% <br />
+					- outputfile (optional string): The file that will be generated. Writes to stdout if not provided. <br />
+					- templatepath (string): The directory that contains all template files. <br />
+					- adjustspacing (boolean, default: true): Specifies whether to ensure exactly one newline between rendered template files. If false, then the rendering will default to MiniJinja's behaviour. <br />
+					- verifycontent (boolean, default: true): Whether to report differences from an already existing rendered file. Will ask before replacing with the new content. Set to false to overwrite existing file without checking for changes. <br />
+					- counters (optional list of counter structs): Contains a list of global auto-incrementing counter functions. <br />
+					- sources (list of source structs): Contains a list of source file configurations. <br />
+					- layout (list of template structs): Contains a list of templates in the order they should be rendered. <br />
+					Instructions:
+					- Use the available tools to get the correct information to fullfill the user request. <br />
+					- Answer only based on the information in the context. <br />
+					- Always use jinja2 formatting in the templates. <br />
+					{'<' + "Active SCG context configuration" + '>'}<br />
+					{fileText}<br />
+					{'</' + "Active SCG context configuration" + '>'}<br />
+				</UserMessage>
 		);
 	}
 }
