19737edec163780fd4cf01e1016f1812d44cc333
Vetle Koppergård <89847536+vetlek@users.noreply.github.com>
2024-12-06 14:20:30 +0100

    Add read scg source tool

--- client/src/tools.ts
+++ client/src/tools.ts
@@ -4,11 +4,13 @@ import {
 	LanguageClient,
 } from "vscode-languageclient/node";
 import * as protocol from "./protocol";
+import * as YAML from "js-yaml";
 
 export function registerChatTools(context: vscode.ExtensionContext, client: LanguageClient) {
 	context.subscriptions.push(vscode.lm.registerTool("septic-tools_validate_calculation", new ValidateCalculationTool(client)));
 	context.subscriptions.push(vscode.lm.registerTool("septic-tools_get_functions", new GetFunctions(client)));
 	context.subscriptions.push(vscode.lm.registerTool("septic-tools_get_variables", new GetVariables(client)));
+	context.subscriptions.push(vscode.lm.registerTool("septic-tools_get_scg_source", new GetScgSource(client)));
 }
 
 interface IValidateCalculationParameters {
@@ -103,3 +105,67 @@ export class GetVariables implements vscode.LanguageModelTool<object> {
 	}
 }
 
+interface IGetScgSourceParameters {
+	sourceId: string;
+}
+
+interface ScgConfig {
+	outputfile?: string;
+	templatepath: string;
+	adjustspacing: boolean;
+	verifycontent: boolean;
+	counters?: {
+		name: string;
+		value: number;
+	}[]
+	sources: {
+		filename: string;
+		id: string;
+		sheet?: string;
+		delimiter?: string;
+	}[]
+	layout: {
+		name: string;
+		source?: string;
+		include?: object;
+	}[]
+}
+
+export class GetScgSource implements vscode.LanguageModelTool<IGetScgSourceParameters> {
+	private client: LanguageClient;
+	constructor(client: LanguageClient) {
+		this.client = client;
+	}
+
+	async invoke(
+		options: vscode.LanguageModelToolInvocationOptions<IGetScgSourceParameters>,
+		_token: vscode.CancellationToken
+	) {
+		const uri = vscode.window.activeTextEditor?.document.uri.toString();
+		const context = await this.client.sendRequest(protocol.getContext, { uri: uri });
+		const fileContent = await vscode.workspace.fs.readFile(vscode.Uri.parse(context));
+		const scg = YAML.load(fileContent.toString()) as ScgConfig;
+		const source = scg.sources.find(source => source.id === options.input.sourceId);
+		if (!source) {
+			return new vscode.LanguageModelToolResult([new vscode.LanguageModelTextPart(`No scg source found with the id ${options.input.sourceId}`)]);
+		}
+		if (!source.filename.endsWith('.csv')) {
+			return new vscode.LanguageModelToolResult([new vscode.LanguageModelTextPart(`The source ${source.filename} is not a csv file`)]);
+		}
+		const contextUri = vscode.Uri.parse(context);
+		const contextDir = vscode.Uri.joinPath(contextUri, '..');
+		const sourcePath = vscode.Uri.joinPath(contextDir, source.filename);
+		const sourceContent = await vscode.workspace.fs.readFile(sourcePath);
+		const fileContentString = Buffer.from(sourceContent).toString('utf8');
+		return new vscode.LanguageModelToolResult([new vscode.LanguageModelTextPart(`Content of the source file ${options.input.sourceId} in csv format: \n ${fileContentString}`)]);
+	}
+
+	async prepareInvocation(
+		options: vscode.LanguageModelToolInvocationPrepareOptions<IGetScgSourceParameters>,
+		_token: vscode.CancellationToken
+	) {
+		return {
+
+		};
+	}
+}
