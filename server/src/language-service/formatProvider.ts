82d29307a295c2712b8bbe35f807ac6183fd00ab
Vetle Koppergård <89847536+vetlek@users.noreply.github.com>
2025-03-25 10:58:14 +0100

    Add test for formatting of calcs with jinja

--- server/src/language-service/formatProvider.ts
+++ server/src/language-service/formatProvider.ts
@@ -31,7 +31,6 @@ export const jinjaIfRegex = /^\{%-?\s+if\b.+%}$/;
 export const jinjaForEndRegex = /\{%-?\s+endfor\s+%}$/;
 export const jinjaIfEndRegex = /\{%-?\s+endif\s+%}$/;
 export const jinjaExpressionRegex = /\{%[\S\s]*%\}/;
-export const endsWithJinjaExpressionRegex = /\{%[\S\s]*%\}\s*$/;
 export const stopFormattingRegex = /^\{#\s+format:off\s+#}$/;
 export const startFormattingRegex = /^\{#\s+format:on\s+#}$/;
 export const lineCommentRegex = /^\s*\/\/\s|\*\/\s*$|#}\s*$|%}\s*$/;
