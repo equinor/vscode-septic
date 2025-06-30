8a05ae512d7d62289b864bbaff97b3de4dc961a6
Vetle Koppergård <89847536+vetlek@users.noreply.github.com>
2025-06-30 12:19:06 +0200

    Fix non valid path github scripts

--- scripts/src/github.py
+++ scripts/src/github.py
@@ -49,8 +49,7 @@ def get_dir(ref: str, dir: str):
 
 def get_object_files(ref: str):
     paths_src = get_dir(ref, "src")
-    paths_fmu = get_dir(ref, "src/fmusrc")
-    paths = paths_src + paths_fmu
+    paths = paths_src
     paths = [x["path"] for x in paths if x["path"].endswith(".cpp")]
     for path in paths:
         try:
