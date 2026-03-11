b16fb3ad637552431c0d732d4223cfb67716ad4c
Vetle Koppergård <89847536+vetlek@users.noreply.github.com>
2026-03-11 14:49:28 +0100

    Fix settings for extensions

--- packages/septic/eslint.config.mjs
+++ packages/septic/eslint.config.mjs
@@ -1,28 +1,23 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    ignores: [
-        "**/out",
-        "**/dist",
-        "**/*.d.ts",
-        ".vscode-test/**/*"
-    ],
-    languageOptions: {
-      parserOptions: {
-        tsconfigRootDir: __dirname,
-      },
+export default defineConfig(
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    {
+        ignores: ["**/out", "**/dist", "**/*.d.ts", ".vscode-test/**/*"],
+        languageOptions: {
+            parserOptions: {
+                tsconfigRootDir: __dirname,
+            },
+        },
     },
-  }
-
 );
